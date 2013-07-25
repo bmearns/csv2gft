706508c580c85cf27113ce941a541400709600a3
Ben Mearns <mearns@udel.edu>
2013-07-25 16:26:26 -0400

    Restored credentials.sh and removed header row from csv data.

---
+++ ftapi/credentials.sh
@@ -0,0 +1,13 @@
+# This file contains sensitive information and should have permissions
+# rw-------. It will be sourced and rewritten by other scripts in this
+# directory. The values for the first three lines come from the API Access page
+# of the Google API console (https://code.google.com/apis/console).
+CLIENT_ID=1066354131069-t1srkkmmuabc309vl2f44f2itu1t9nbt.apps.googleusercontent.com
+CLIENT_SECRET=00zF1oJFdmmj6KTM0ZT9U7FQ
+API_KEY=AIzaSyA_Bcq_-jEKafLE0nfdkTNx2Jg7QZDK4Ng
+error="invalid_request"
+token_type="Bearer"
+refresh_token="1/xrL5dg_X0Y2Ccgg2XZ2vKCsblI7Lhv4-xzTVSk4TDAw"
+access_token="ya29.AHES6ZSOzgbshFQrAXpLPPgksSiUs6ay_9dNuTy4TavOyYS5EZqPZXo-oqs"
+expires_in=3600
+expires_at=1374786786
